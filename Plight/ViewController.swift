72927f3f208b85c04db5263c71df32c0c5c5a1aa
mdrafsan <mdrafsan@buffalo.edu>
2018-01-27 15:05:40 -0500

    Initial Commit

---
+++ Plight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plight
+//
+//  Created by Rafsan Chowdhury on 1/27/18.
+//  Copyright © 2018 appimas24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
